0114d13091522803f2f6f407eefbe9a2356963ee
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2018-01-21 22:03:45 +0000

    Draw.io

---
+++ engine/listing/listing.py
@@ -0,0 +1,16 @@
+class ListingEngine(object):
+
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
+
+    def add(self):
+        pass
+
+    def submit(self):
+        pass
+
+    def remove(self):
+        pass
